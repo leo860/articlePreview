46d8dec9447510a075343a61e5da5461fc773951
leo860 <59024138+leo860@users.noreply.github.com>
2024-07-18 13:41:18 -0500

    commit initial

---
+++ main.js
@@ -0,0 +1,4 @@
+var menuShare = document.querySelector("#menu") 
+var shareButton = document.querySelector("#share-button");
+shareButton.addEventListener("click", () => {
+ 	menuShare.style.visibility = 'visible';})
